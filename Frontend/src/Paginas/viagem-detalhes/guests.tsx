006df0db9a5084b1618dbee372bfae83a3ca325e
Matheus Felipe Godoi Coutinho <138553871+matheusfgcz@users.noreply.github.com>
2024-08-30 15:33:29 -0300

    Update Frontend

--- Frontend/src/Paginas/viagem-detalhes/guests.tsx
+++ Frontend/src/Paginas/viagem-detalhes/guests.tsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { api } from "../../lib/axios";
 
-interface participantes {
+interface Membros {
   id: string;
   nome: string | null;
   email: string;
@@ -12,31 +12,31 @@ interface participantes {
 }
 export function Guests() {
   const { idViagem } = useParams();
-  const [participantes, setParticipantes] = useState<participantes[]>([]);
+  const [membros, setMembros] = useState<Membros[]>([]);
   useEffect(() => {
     api
       .get(`/viagem/${idViagem}/participantes`)
-      .then((response) => setParticipantes(response.data.viagem));
+      .then((response) => setMembros(response.data.viagem));
   }, [idViagem]);
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Convidados</h2>
       <div className="space-y-5">
-        {participantes.map((participante, index) => {
+        {membros.map((membro, index) => {
           return (
             <div
-              key={participante.id}
+              key={membro.id}
               className="flex items-center justify-between gap-4"
             >
               <div className="space-y-1.5">
                 <span className="block font-medium text-white">
-                  {participante.nome ?? `Convidado ${index}`}
+                  {membro.nome ?? `Convidado ${index}`}
                 </span>
                 <span className="block text-sm text-white truncate">
-                  {participante.email}
+                  {membro.email}
                 </span>
               </div>
-              {participante.esta_confirmado ? (
+              {membro.esta_confirmado ? (
                 <CheckCircle2 className="text-green-500 size-5 shrink-0" />
               ) : (
                 <CircleDashed className="text-white size-5 shrink-0" />
